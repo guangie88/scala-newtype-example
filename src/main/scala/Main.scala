fa71d2d08e93354ee1da10f62afbedc465711e47
Chen Weiguang <chen.weiguang@gmail.com>
2018-10-03 17:26:51 +0800

    Add Kyro code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,37 @@
 import io.estatico.newtype.ops._
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
 import types._
 
 object Main extends App {
   def printBase[T](x: T) = println(x)
   def printAdv[T](x: Adv[T]) = println(x)
 
-  val v = Adv[Int](777)
+  val x = Adv[Int](777)
 
   // this will fail which is good
-  // printBase(v)
+  // printBase(x)
 
   // this will work
-  printAdv(v)
+  printAdv(x)
 
   // for coercing into base type
-  printBase(v.coerce[Int])
+  printBase(x.coerce[Int])
+
+  // for Kryo serialization
+  val kryo = new Kryo()
+  val byteArr = new Array[Byte](256)
+
+  val output = new Output(byteArr);
+  kryo.writeObject(output, x)
+  output.close()
+
+  val input = new Input(byteArr);
+  val y = kryo.readObject(input, classOf(Adv[Int].Type))
+  input.close()
+
+  println(y)
 }
