35c08b90f0fbdb6b7ee8005c850489d0b2b074df
Weiguang <chen.weiguang@gmail.com>
2018-10-04 08:48:29 +0800

    Add Kryo to test serialization usability (#1)
    * Add Kyro code
    
    * Make things work
    
    * Fix syntax error

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,50 @@
 import io.estatico.newtype.ops._
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
 import types._
 
 object Main extends App {
-  def printBase[T](x: T) = println(x)
-  def printAdv[T](x: Adv[T]) = println(x)
+  def printInt(x: Int) = println(s"${x.getClass}: $x")
+  def printAdv[T](x: Adv[T]) = println(s"${x.getClass}: $x")
 
-  val v = Adv[Int](777)
+  val x = Adv[Int](777)
 
   // this will fail which is good
-  // printBase(v)
+  // printInt(x)
 
   // this will work
-  printAdv(v)
+  printAdv(x)
 
   // for coercing into base type
-  printBase(v.coerce[Int])
+  printInt(x.coerce[Int])
+  printInt(x.value)
+
+  // for Kryo serialization
+  val kryo = new Kryo()
+  val byteArr = new Array[Byte](256)
+
+  val output = new Output(byteArr);
+  kryo.writeObject(output, x)
+  output.close()
+
+  val inputY = new Input(byteArr);
+  // weird syntax, it's Adv.Repr[Int] instead of Adv[Int].Repr
+  val y = Adv[Int](kryo.readObject(inputY, classOf[Adv.Repr[Int]]))
+  inputY.close()
+
+  // this will fail which is good
+  // printInt(y)
+
+  // this will work
+  printAdv(y)
+
+  // reading into primitive works too
+  val inputZ = new Input(byteArr);
+  val z = kryo.readObject(inputZ, classOf[Int])
+  inputZ.close()
+
+  printInt(z)
 }
--- src/main/scala/Types.scala
+++ src/main/scala/Types.scala
@@ -2,4 +2,5 @@ import io.estatico.newtype.macros.newtype
 
 package object types {
   @newtype case class Adv[T](value: T)
+  @newtype case class AdvInt(value: Int)
 }
